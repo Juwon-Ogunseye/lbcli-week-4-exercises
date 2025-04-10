db7e99d7c094bbbbd192b618e35fd45e92b3fb3c
Ogunseye Oluwajuwon <ogunseyemicheal@gmail.com>
2025-04-10 14:00:37 +0100

    finally

--- submission/02.sh
+++ submission/02.sh
@@ -1,20 +1,18 @@
 #!/bin/bash
 
-# UTXO from the transaction provided
-TXID="72efbd235f00d1d92c77e3e4b2a5f6d9954547d0865f8d695eecbbde8e92b0c8"
-VOUT=0
-
-# Output address and amount (20,000,000 satoshis)
-RECEIVER="2MvLcssW49n9atmksjwg2ZCMsEMsoj3pzUP"
-AMOUNT="0.2"  # in BTC (which is 20,000,000 satoshis)
-
-# Locktime set for 2 weeks from current block height (25)
+# Define variables for the txid, vouts, recipient address, and amount
+TXID="23c19f37d4e92e9a115aab86e4edc1b92a51add4e0ed0034bb166314dde50e16"
+VOUT0=0
+VOUT1=1
+RECIPIENT="2MvLcssW49n9atmksjwg2ZCMsEMsoj3pzUP"
+AMOUNT=0.2
 LOCKTIME=2041
 
-# Create raw transaction using bitcoin-cli
+# Create the raw transaction using the provided inputs
 RAW_TX=$(bitcoin-cli -regtest createrawtransaction \
-'[{"txid":"'"$TXID"'","vout":'"$VOUT"'}]' \
-'{"'"$RECEIVER"'":'"$AMOUNT"'}' $LOCKTIME)
+  "[{\"txid\":\"$TXID\", \"vout\":$VOUT0}, {\"txid\":\"$TXID\", \"vout\":$VOUT1}]" \
+  "{\"$RECIPIENT\":$AMOUNT}" \
+  $LOCKTIME)
 
 # Output the raw transaction
 echo "Raw Transaction: $RAW_TX"
